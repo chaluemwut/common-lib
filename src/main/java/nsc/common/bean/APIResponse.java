9dae0843d2b4c4546285722eddebfe9c9c5c527a
chaluemwuut <chaluemwuut@iMac-khxng-chaluemwuut.local>
2018-04-06 20:15:37 +0700

    update apiresponse

--- src/main/java/nsc/common/bean/APIResponse.java
+++ src/main/java/nsc/common/bean/APIResponse.java
@@ -4,6 +4,7 @@ public class APIResponse {
 	private int status = 0;
 	private String message = "Ok request";
 	private Object data;
+	private boolean isExpire;
 	
 	public int getStatus() {
 		return status;
@@ -23,4 +24,12 @@ public class APIResponse {
 	public void setData(Object data) {
 		this.data = data;
 	}
+
+    public boolean isExpire() {
+        return isExpire;
+    }
+
+    public void setExpire(boolean expire) {
+        isExpire = expire;
+    }
 }
